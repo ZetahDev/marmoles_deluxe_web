007b28a035adb6d456965983b2792cc81d9429a9
ZetahDev <zetahdev@gmail.com>
2025-05-02 14:59:58 -0500

    Add authentication integration and refactor AdminLayout for session management
    - Integrated `auth-astro` for user authentication in the admin layout.
    - Implemented session checking with redirection to the login page if the user is not authenticated.
    - Updated layout structure for improved navigation and user experience.
    - Enhanced HTML structure and accessibility features in the AdminLayout component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), auth()],
   site: 'https://marmolesdeluxe.com',
   vite: {
     build: {
