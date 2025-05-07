65207afa02ef284e6d0e1d21b6a13e57c9ee6fd3
ZetahDev <zetahdev@gmail.com>
2025-05-06 20:27:15 -0500

    Update .gitignore and clean up Astro configuration
    - Added .vscode/ and .vercel/ to .gitignore to prevent unnecessary files from being tracked.
    - Removed unused Vercel adapter import and adjusted the Astro configuration for better clarity and organization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,7 +15,5 @@ export default defineConfig({
         }
       }
     }
-  },
-  output: 'static',
-  adapter: vercel(),
+  }
 });
