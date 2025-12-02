e79b4e59d5ec25d68201ff7b77fa7e6eafcaa7f2
ZetahDev <zetahdev@gmail.com>
2025-12-02 18:02:52 -0500

    feat: Enhance performance by implementing critical CSS for hero section and updating client load strategies for components

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,14 +7,31 @@ import vercel from "@astrojs/vercel";
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
   site: "https://marmolesdeluxe.com",
   vite: {
     build: {
       assetsDir: "assets",
+      cssCodeSplit: true,
       rollupOptions: {
         output: {
           assetFileNames: "assets/[name].[hash][extname]",
+          manualChunks: (id) => {
+            if (id.includes("node_modules")) {
+              if (id.includes("react") || id.includes("react-dom")) {
+                return "vendor-react";
+              }
+              if (id.includes("@radix-ui")) {
+                return "vendor-ui";
+              }
+              return "vendor";
+            }
+          },
         },
       },
     },
